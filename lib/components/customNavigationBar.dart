462ffab5d906e224d9a8732ae4b3185cc9e0d3d2
hayward2007 <hayward2007@naver.com>
2024-09-11 16:26:11 +0900

    fix: fixed overflow of bottom navigate bar

--- lib/components/customNavigationBar.dart
+++ lib/components/customNavigationBar.dart
@@ -4,66 +4,63 @@ import 'package:get/get.dart';
 class CustomNaviationBar extends StatelessWidget {
   final int currentIndex;
 
-  CustomNaviationBar({required this.currentIndex});
-
+  const CustomNaviationBar({super.key, required this.currentIndex});
 
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
           color: Colors.white,
-          border: Border.all(color: Color.fromRGBO(184, 184, 184, 1.0), width: 0.5),
+          border: Border.all(color: const Color.fromRGBO(184, 184, 184, 1.0), width: 0.5),
         ),
-        height: 70.0, // BottomNavigationBar의 높이 조정
+        // height: 70.0, // BottomNavigationBar의 높이 조정
         child: ClipRRect(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          currentIndex: currentIndex,
-          selectedItemColor: Color.fromRGBO(54, 54, 54, 1.0),
-          unselectedItemColor: Color.fromRGBO(184, 184, 184, 1.0),
-          selectedLabelStyle: TextStyle(fontSize: 12),
-          unselectedLabelStyle: TextStyle(fontSize: 12),
-          type: BottomNavigationBarType.fixed,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.house_rounded, size:28),
-              label: '랭킹',
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bar_chart_rounded, size:28),
-              label: '투자',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.payments_rounded, size:28),
-              label: '은행',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.star_rounded, size:28),
-              label: '랭킹',
-            ),
-          ],
-          onTap: (int index) {
-            switch (index) {
-              case 0:
-                Get.offNamed('/');
-                break;
-              case 1:
-                Get.offNamed('/');
-                break;
-              case 2:
-                Get.offNamed('/video');
-                break;
-              case 3:
-                Get.offNamed('/ranking');
-                break;
-            }
-          },
-        )
-      )
-    );
+            child: BottomNavigationBar(
+              backgroundColor: Colors.white,
+              currentIndex: currentIndex,
+              selectedItemColor: const Color.fromRGBO(54, 54, 54, 1.0),
+              unselectedItemColor: const Color.fromRGBO(184, 184, 184, 1.0),
+              selectedLabelStyle: const TextStyle(fontSize: 12),
+              unselectedLabelStyle: const TextStyle(fontSize: 12),
+              type: BottomNavigationBarType.fixed,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.house_rounded, size: 28),
+                  label: '랭킹',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bar_chart_rounded, size: 28),
+                  label: '투자',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.payments_rounded, size: 28),
+                  label: '은행',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.star_rounded, size: 28),
+                  label: '랭킹',
+                ),
+              ],
+              onTap: (int index) {
+                switch (index) {
+                  case 0:
+                    Get.offNamed('/');
+                    break;
+                  case 1:
+                    Get.offNamed('/');
+                    break;
+                  case 2:
+                    Get.offNamed('/video');
+                    break;
+                  case 3:
+                    Get.offNamed('/ranking');
+                    break;
+                }
+              },
+            )));
   }
 }
