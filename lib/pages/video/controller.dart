e7dbbfbe99310f2510a036a1bcd1e4ec3b4ad164
hayward2007 <hayward2007@naver.com>
2024-09-18 22:30:51 +0900

    feat: added quiz class

--- lib/pages/video/controller.dart
+++ lib/pages/video/controller.dart
@@ -2,4 +2,13 @@ import 'package:get/get.dart';
 
 class VideoPageController extends GetxController {
   static VideoPageController get to => Get.find<VideoPageController>();
+
+  final RxString pressedButton = "".obs;
+  final RxBool _isQuizTime = false.obs;
+
+  get isQuizTime => _isQuizTime.value;
+
+  void startQuiz() {
+    _isQuizTime.value = true;
+  }
 }
