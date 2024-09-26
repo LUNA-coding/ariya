9c12cedbaf60f8bd470e34937680b13489bd49a5
hayward2007 <hayward2007@naver.com>
2024-09-26 17:45:28 +0900

    feat: added select quiz

--- lib/global.dart
+++ lib/global.dart
@@ -7,5 +7,7 @@ class AriyaColor {
   static const Color white = Color.fromRGBO(255, 255, 255, 1);
   static const Color grayscale100 = Color.fromRGBO(245, 245, 245, 1);
   static const Color grayscale200 = Color.fromRGBO(235, 235, 235, 1);
+  static const Color grayscale300 = Color.fromRGBO(219, 219, 219, 1);
   static const Color grayscale400 = Color.fromRGBO(184, 184, 184, 1);
+  static const Color grayscale800 = Color.fromRGBO(79, 79, 79, 1);
 }
--- lib/pages/video/controller.dart
+++ lib/pages/video/controller.dart
@@ -1,5 +1,7 @@
 import 'package:ariya/pages/video/widgets/quiz_result.dart';
+import 'package:ariya/pages/video/widgets/select_quiz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'dart:async';
 
@@ -9,11 +11,38 @@ import 'package:ariya/widgets/button_controller.dart';
 class VideoPageController extends GetxController with ButtonController {
   static VideoPageController get to => Get.find<VideoPageController>();
   static final List<Widget> _quiz_list = [
-    OxQuiz(question: "보통의 경우,\n채권이 주식보다\n변동성이 크다.", answer: 'O'),
-    OxQuiz(question: "채권의 수익은\n‘이자'라는 형태로\n발생한다.", answer: 'X'),
-    OxQuiz(question: "주식과 채권은 모두\n투자 상품이다.\n", answer: 'X'),
-    OxQuiz(question: "‘채권 매수'란,\n회사에 돈을\n빌려주는 것을 말한다.", answer: 'X'),
-    OxQuiz(question: "‘주식’은\n회사의 소유권을\n나타낸다.", answer: 'X'),
+    SelectQuiz(
+      question: "두 명의 사람이 사과를 먹고 싶은데,\n남은 사과가 하나 밖에 없습니다.\n사과의 가격은 어떻게 변할까요?",
+      answer: 1,
+      choice: const ["올라간다", "내려간다"],
+      image: [
+        SvgPicture.asset('assets/images/quiz/up.svg', height: 76, width: 76),
+        SvgPicture.asset('assets/images/quiz/down.svg', height: 76, width: 76),
+      ],
+    ),
+    SelectQuiz(
+      question: "당신은 안전한 투자를 중요하게 생각\n하는 투자자입니다. 다음 중 어떤 투자\n포트폴리오를 구성해야할까요?",
+      answer: 1,
+      choice: const ["주식:채권 = 7:3", "주식:채권 = 3:7"],
+      image: [
+        SvgPicture.asset('assets/images/quiz/seven_to_three.svg', height: 76, width: 76),
+        SvgPicture.asset('assets/images/quiz/three_to_seven.svg', height: 76, width: 76),
+      ],
+    ),
+    SelectQuiz(
+      question: "당신은 고위험, 고수익 투자를\n지향하는 투자자입니다.\n둘 중 어떤 상품을 매수해야 할까요?",
+      answer: 1,
+      choice: const ["주식", "채권"],
+      image: [
+        SvgPicture.asset('assets/images/quiz/stock.svg', height: 76, width: 76),
+        SvgPicture.asset('assets/images/quiz/bond.svg', height: 76, width: 76),
+      ],
+    ),
+    OxQuiz(question: "보통의 경우,\n채권이 주식보다\n변동성이 크다.", answer: 'X'),
+    OxQuiz(question: "채권의 수익은\n‘이자'라는 형태로\n발생한다.", answer: 'O'),
+    OxQuiz(question: "주식과 채권은 모두\n투자 상품이다.\n", answer: 'O'),
+    OxQuiz(question: "‘채권 매수'란,\n회사에 돈을\n빌려주는 것을 말한다.", answer: 'O'),
+    OxQuiz(question: "‘주식’은\n회사의 소유권을\n나타낸다.", answer: 'O'),
   ];
 
   final RxInt _quizIndex = 0.obs;
@@ -22,12 +51,13 @@ class VideoPageController extends GetxController with ButtonController {
   final Rx<bool?> _quizStatus = Rx(null);
   final Rx<Widget> quiz = _quiz_list[0].obs;
 
-  get quizAnswer => _quizAnswer.value;
-
-  get isQuizTime => _isQuizTime.value;
+  String get quizAnswer => _quizAnswer.value;
+  bool get isQuizTime => _isQuizTime.value;
   bool get isQuizSolved => _quizStatus.value != null;
   bool get isQuizCorrect => _quizStatus.value == true;
   bool get isQuizInCorrect => _quizStatus.value == false;
+
+  set quizAnswer(String value) => _quizAnswer.value = value;
 
   void startQuiz() => _isQuizTime.value = true;
   void _refreshQuiz() {
@@ -46,6 +76,7 @@ class VideoPageController extends GetxController with ButtonController {
         return;
       }
       quiz.value = _quiz_list[_quizIndex.value];
+      Get.put(VideoPageController());
     });
   }
 }
