dd842c3f44841cd7f986d3feabe008fdaa6938ae
hayward2007 <hayward2007@naver.com>
2024-10-16 20:46:49 +0900

    fix: fixed video

--- lib/pages/video/page.dart
+++ lib/pages/video/page.dart
@@ -39,60 +39,69 @@ class VideoPage extends GetView<VideoPageController> {
         scrolledUnderElevation: 0,
         title: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          child: Column(children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              GestureDetector(
-                onTap: () => Get.offAllNamed(Routes.HOME),
-                child: const Icon(
-                  Icons.close_rounded,
-                  color: Color.fromRGBO(79, 79, 79, 1),
-                ),
-              ),
-              Obx(() => LinearPercentIndicator(
-                    lineHeight: 8.0,
-                    animationDuration: 1000,
-                    percent: VideoPageController.to.process,
-                    barRadius: const Radius.circular(99),
-                    progressColor: const Color.fromRGBO(156, 112, 213, 1),
-                    backgroundColor: const Color.fromRGBO(235, 235, 235, 1),
-                    width: 220,
-                  )),
-              Container(
-                width: 70,
-                height: 32,
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                clipBehavior: Clip.antiAlias,
-                decoration: ShapeDecoration(
-                  color: const Color(0xFFF4F4F4),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(7.68),
-                  ),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: Obx(() => Text(
-                            formatDuration(seconds.value),
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: Color(0xFF4F4F4F),
-                              fontSize: 16,
-                              fontFamily: 'Spoqa Han Sans Neo',
-                              fontWeight: FontWeight.w500,
-                              height: 0,
-                              letterSpacing: -0.48,
-                            ),
-                          )),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.offAllNamed(Routes.HOME),
+                    child: const Icon(
+                      Icons.close_rounded,
+                      color: Color.fromRGBO(79, 79, 79, 1),
                     ),
-                  ],
-                ),
-              )
-            ]),
-          ]),
+                  ),
+                  Obx(
+                    () => LinearPercentIndicator(
+                      lineHeight: 8.0,
+                      animationDuration: 1000,
+                      percent: VideoPageController.to.process,
+                      barRadius: const Radius.circular(99),
+                      progressColor: const Color.fromRGBO(156, 112, 213, 1),
+                      backgroundColor: const Color.fromRGBO(235, 235, 235, 1),
+                      width: 220,
+                    ),
+                  ),
+                  Container(
+                    width: 70,
+                    height: 32,
+                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFFF4F4F4),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7.68),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: Obx(
+                            () => Text(
+                              formatDuration(seconds.value),
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Color(0xFF4F4F4F),
+                                fontSize: 16,
+                                fontFamily: 'Spoqa Han Sans Neo',
+                                fontWeight: FontWeight.w500,
+                                height: 0,
+                                letterSpacing: -0.48,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       body: Stack(
@@ -120,8 +129,23 @@ class VideoPage extends GetView<VideoPageController> {
                   ),
                 ),
                 clipBehavior: Clip.antiAlias,
-                child: Stack(
+                child: Column(
                   children: [
+                    GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () => VideoPageController.to.isVideoDown.value = !VideoPageController.to.isVideoDown.value,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(VideoPageController.to.isVideoDown.value ? '영상 다시 보기' : '문제 풀러 가기',
+                                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, letterSpacing: -0.3, color: AriyaColor.white)),
+                            Icon(VideoPageController.to.isVideoDown.value ? Icons.arrow_upward_rounded : Icons.arrow_downward_rounded, color: AriyaColor.white),
+                          ],
+                        ),
+                      ),
+                    ),
                     SizedBox(
                       width: MediaQuery.of(context).size.width,
                       height: MediaQuery.of(context).size.height - 150,
@@ -137,23 +161,6 @@ class VideoPage extends GetView<VideoPageController> {
                         ),
                       ),
                     ),
-                    Positioned(
-                      child: GestureDetector(
-                        behavior: HitTestBehavior.translucent,
-                        onTap: () => VideoPageController.to.isVideoDown.value = !VideoPageController.to.isVideoDown.value,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(VideoPageController.to.isVideoDown.value ? '영상 다시 보기' : '문제 풀러 가기',
-                                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, letterSpacing: -0.3, color: AriyaColor.white)),
-                              Icon(VideoPageController.to.isVideoDown.value ? Icons.arrow_upward_rounded : Icons.arrow_downward_rounded, color: AriyaColor.white),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
                   ],
                 ),
               ),
